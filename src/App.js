b7e18b5118c18e58e96dbdfdee49f6d0e7ff5b53
Haganeken <ktat73@csu.fullerton.edu>
2019-12-05 16:29:56 -0800

    Basic firebase + login authentication added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+import logo from './logo.svg';
 import './App.css';
-import Navbar from './navbar/Navbar.js';
-import Grid from './grid/Grid.js';
 
-class App extends React.Component {
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
+class App extends Component {
+  render() {
+    const {
+      user,
+      signOut,
+      signInWithGoogle,
+    } = this.props;
 
-    render() {
-        return (
-            <div>
-                <Navbar onAddItem={this.onAddItem}/>
-                <Grid onAddItem={this.onAddItem}/>
-            </div>
-        )
-    };
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          {
+            user
+              ? <p>Hello, {user.displayName}</p>
+              : <p>Please sign in.</p>
+          }
+
+          {
+            user
+              ? <button onClick={signOut}>Sign out</button>
+              : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const firebaseAppAuth = firebaseApp.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
