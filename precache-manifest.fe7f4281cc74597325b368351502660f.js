b3fab120db8df4cd2ce6da01ead00129740e7e94
simonmoesorensen <moe.simon@gmail.com>
2019-12-06 15:41:12 -0800

    Updates

---
+++ precache-manifest.fe7f4281cc74597325b368351502660f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a83d89e945df0a2e13901a0ab7cfd54",
+    "url": "/procrastinator/index.html"
+  },
+  {
+    "revision": "6674e5910d0153e1ec48",
+    "url": "/procrastinator/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "13d6b1b38412169ae8f3",
+    "url": "/procrastinator/static/css/main.18ad74a2.chunk.css"
+  },
+  {
+    "revision": "6674e5910d0153e1ec48",
+    "url": "/procrastinator/static/js/2.a7e80ad7.chunk.js"
+  },
+  {
+    "revision": "13d6b1b38412169ae8f3",
+    "url": "/procrastinator/static/js/main.008a035e.chunk.js"
+  },
+  {
+    "revision": "ac6c649e7ac26a0dac67",
+    "url": "/procrastinator/static/js/runtime-main.ba4e26b0.js"
+  }
+]);
