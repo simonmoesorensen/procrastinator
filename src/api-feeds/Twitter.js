f137ae8bbe646bd87ef8ac84ef6af08f727f4b47
Twigy1 <twigy1@csu.fullerton.edu>
2019-12-09 10:00:09 -0800

    twitter update

--- src/api-feeds/Twitter.js
+++ src/api-feeds/Twitter.js
@@ -1,18 +1,58 @@
 import React from "react";
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterFollowButton,
+  TwitterHashtagButton,
+  TwitterMentionButton,
+  TwitterTweetEmbed,
+  TwitterMomentShare,
+  TwitterDMButton,
+  TwitterVideoEmbed,
+  TwitterOnAirButton
+} from 'react-twitter-embed';
+import {InputGroup} from "react-bootstrap";
+import FormControl from "react-bootstrap/FormControl";
+import Dropdown from "react-dropdown";
 
 
 export default class Twitter extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-    }
+    this.state = {value: ''};
 
-    render() {
-        return (
-            <div>
-                I am Twitter
-            </div>
-        );
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
+
+
+
+  render() {
+    return (
+
+      <div>
+        <div className="selfCenter standardWidth">
+          <TwitterTimelineEmbed
+          sourceType= "profile"
+          screenName= "Twigy1_"
+          options={{height: 250}}
+          />
+        </div>
+
+      </div>
+
+    );
+  }
 
 }
